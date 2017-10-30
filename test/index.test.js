f4733959d5ceade9c5cad240247dc35e86b1f7da
FengYuHe <heyufeng8080@gmail.com>
2017-10-30 11:29:48 +0800

    add test

---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+const test = require('ava');
+const path = require('path');
+const shortid = require('shortid');
+const SIMPLE_APP = path.join(__dirname, 'simple-app');
+
+const app = require(path.join(SIMPLE_APP, 'server/server.js'));
+const Widget = app.models.Widget;
+
+test(async t => {
+  const widget = await Widget.create({name: 'foo', type: 'bar'});
+  t.true(shortid.isValid(widget.id));
+});
