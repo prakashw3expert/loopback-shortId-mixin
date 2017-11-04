9206a41af6d1d918b8fea39bcb2c8193d0966dad
Prakash Saini <prakashw3expert@gmail.com>
2017-11-04 17:26:53 +0530

    Update index.js
    Fixed comfortability with loopback count mixin

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = function mixin (app) {
-  app.loopback.modelBuilder.mixins.define('ShortId', require('./counts'));
+  //app.loopback.modelBuilder.mixins.define('ShortId', require('./counts'));
 };
